edd010315840522156f6f7578fde3d6ff5e9f62c
LukeLauk <111269328+LukeLauk@users.noreply.github.com>
2022-10-18 02:47:33 -0400

    Add files via upload

---
+++ Alarm clock/js/script.js
@@ -0,0 +1,109 @@
+// variables
+var time, alarm, currentH, currentM,
+    activeAlarm = false,
+    sound = new Audio("https://cdn.freesound.org/previews/553/553932_12383923-lq.mp3");
+
+/*
+  audio source: https://freesound.org/s/553932/
+*/
+
+// loop alarm
+sound.loop = true;
+
+// function to display the current time
+function displayTime() {
+  var now = new Date();
+  time = now.toLocaleTimeString();
+  clock.textContent = time;
+  // time = "1:00:00 AM";
+  // watch for alarm
+  if (time === alarm) {
+    sound.play();
+    
+    // snooze button
+    snooze.className = "";
+  }
+  setTimeout(displayTime, 1000);
+}
+displayTime();
+
+// add option values relative towards time
+function addMinSecVals(id) {
+  var select = id;
+  var min = 59;
+  
+  for (i = 0; i <= min; i++) {
+    // defined as new Option(text, value)
+    select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i < 10 ? "0" + i : i);
+  }
+}
+function addHours(id) {
+  var select = id;
+  var hour = 12;
+  
+  for (i = 1; i <= hour; i++) {
+    // defined as new Option(text, value)
+    select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+  }
+}
+addMinSecVals(minutes);
+addMinSecVals(seconds);
+addHours(hours);
+
+// set and clear alarm
+startstop.onclick = function() {
+  // set the alarm
+  if (activeAlarm === false) {
+    hours.disabled = true;
+    minutes.disabled = true;
+    seconds.disabled = true;
+    ampm.disabled = true;
+    
+    alarm = hours.value + ":" + minutes.value + ":" + seconds.value + " " + ampm.value;
+    this.textContent = "Clear Alarm";
+    activeAlarm = true;
+  } else {
+    // clear alarm
+    hours.disabled = false;
+    minutes.disabled = false;
+    seconds.disabled = false;
+    ampm.disabled = false;
+    
+    sound.pause();
+    alarm = "00:00:00 AM";
+    this.textContent = "Set Alarm";
+    
+    // hide snooze 
+    snooze.className = "hide";
+    activeAlarm = false;
+  }
+};
+
+// snooze for 5 minutes
+snooze.onclick = function() {
+  if (activeAlarm === true) {
+    // grab the current hour and minute
+    currentH = time.substr(0, time.length - 9);
+    currentM = time.substr(currentH.length + 1, time.length - 8);
+    
+    if (currentM >= "55") {
+      minutes.value = "00";
+      hours.value = parseInt(currentH) + 1;
+    } else {
+      if (parseInt(currentM) + 5 <= 9) {
+        minutes.value = "0" + parseInt(currentM + 5);
+      } else {
+        minutes.value = parseInt(currentM) + 5;
+      }
+    }
+    
+    // hide snooze 
+    snooze.className = "hide";
+    
+    // reset alarm
+    startstop.click();
+    startstop.click();
+  } else {
+    return false;
+  }
+};
